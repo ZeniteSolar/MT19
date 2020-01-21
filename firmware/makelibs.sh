51281f8bc59398f29a351b4b3dc72ba9e1eef9bb
João Antônio Cardoso <joao.maker@gmail.com>
2020-01-21 13:37:39 -0300

    makelibs updated

--- firmware/makelibs.sh
+++ firmware/makelibs.sh
@@ -2,6 +2,4 @@ cd lib/avr-can-lib/src/
 make clean
 make lib
 cd ../../../
-cd src
-wget https://raw.githubusercontent.com/ZeniteSolar/CAN_IDS/master/can_ids.h
-cd ..
+wget https://raw.githubusercontent.com/ZeniteSolar/CAN_IDS/master/can_ids.h -O src/can_ids.h
