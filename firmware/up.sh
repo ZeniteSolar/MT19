00ad68ccd261796dfedec5d145e49500088343ad
alejopm03 <perdomomilar@gmail.com>
2021-12-17 12:05:09 -0300

    add serial output in up script

--- firmware/up.sh
+++ firmware/up.sh
@@ -1,8 +1,9 @@
-
+#!/bin/sh
 #cppcheck  --force --std=c99 --language=c src/
 make clean
 rm -rf bin obj
 make all
-#fuser -k /dev/ttyACM0
-avrdude -c arduino -P COM5 -p m328p -D -U flash:w:bin/firmware.elf
-#figlet "MVC19"
+#fuser -k /dev/ttyUSB0
+avrdude -v -c arduino -P /dev/ttyACM0 -p m328p -D -U flash:w:bin/firmware.elf
+figlet "MCC19"
+stty 57600 < /dev/ttyACM0 && cat /dev/ttyACM0
